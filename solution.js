c94ac559e13640c248f617c5c2ff4309640734e9
Elena Rotari <elena.rotari@icloud.com>
2022-11-29 14:07:00 +0100

    done

---
+++ solution.js
@@ -0,0 +1,25 @@
+import express, { request, response } from "express";
+import { db } from "./db.js";
+const app = express();
+
+app.get("/users", (req, res) => {
+  const user = db.find((el) => el.first_name === req.query.name);
+
+  if (user) {
+    res.status(200).send(user);
+  } else {
+    res.status(200).send(db);
+  }
+});
+
+app.get("/:id", (req, res) => {
+  const user = db.find((el) => el.id === +req.params.id);
+  if (user) {
+    res.status(200).send(user);
+  } else {
+    res.status(404).send(" user not found");
+  }
+});
+app.listen(3002, () => {
+  console.log(" hey i am running on port 3002");
+});
